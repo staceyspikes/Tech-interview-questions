699be92221b3a519a6b028a8d8d9d69523a3d045
staceyspikesgit config --global user.name staceyspikes <stacey.lynne13@yahoo.com>
2017-08-22 18:14:19 -0400

    First Commit

---
+++ Tech interview questions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tech_interview_questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FIZZ BUZZ");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("FIZZ");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("BUZZ");
+            }
+
+            else 
+            {
+                Console.WriteLine("Your number is not divisible by 3 or 5");
+            }
+                        
+        }
+    }
+}
